82605556b856ec8710392d3919dce28c11272831
Cyril <cyril@joshfire.com>
2019-10-30 16:35:54 +0100

    Add delete modals

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,18 @@
 'use strict'
 
 $(document).ready(function() {
-    console.log('start')
+    // For messages
     $('.toast').toast('show');
+
+    // "Delete" modals
+    $('a.delete-modal').click(function() {
+        // Grab real target url for deletion
+        let targetUrl = $(this).attr('data-href');
+
+        // Put it into the modal's OK button
+        $('#delete .target-url').attr('href', targetUrl);
+
+        // Show the modal
+        $('#delete').modal('show');
+    })
 })
