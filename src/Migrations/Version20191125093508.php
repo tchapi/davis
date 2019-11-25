fdedf1c05f29e5f47790032ec44e20804ce678c1
Cyril <cyril@joshfire.com>
2019-11-25 10:58:19 +0100

    Add the isAdmin property migration file

---
+++ src/Migrations/Version20191125093508.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Add the isAdmin property on the principals
+ */
+final class Version20191125093508 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Add the isAdmin property on the principals';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE principals ADD is_admin TINYINT(1) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE principals DROP is_admin');
+    }
+}
