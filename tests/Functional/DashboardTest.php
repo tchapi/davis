1cc5b2e5b773eb55628f00826918c41f90bfde0c
tchapi <regbasket+github@gmail.com>
2022-01-04 20:04:54 +0100

    Add first tests for dashboard pages

---
+++ tests/Functional/DashboardTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Tests\Functional;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DashboardTest extends WebTestCase
+{
+    public function testIndexPage(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h3', 'Davis is running.');
+
+        $this->assertSelectorExists('div.caldav');
+        $this->assertSelectorExists('div.carddav');
+        $this->assertSelectorExists('div.webdav');
+    }
+
+    public function testDashboardPage(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/dashboard');
+
+        $this->assertResponseRedirects('/login');
+    }
+
+    public function testLoginPage(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/login');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Please sign in');
+        $this->assertSelectorExists('nav.navbar');
+    }
+
+    public function testLoginIncorrectUsername(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/login');
+
+        $form = $crawler->selectButton('Submit')->form();
+        $form['username']->setValue('bad_'.$_ENV['ADMIN_LOGIN']);
+        $form['password']->setValue('bad_password');
+
+        $client->submit($form);
+        $this->assertResponseRedirects('/login');
+        $crawler = $client->followRedirect();
+        $this->assertResponseIsSuccessful();
+
+        $this->assertSelectorTextContains('div.alert.alert-danger', 'Username could not be found.');
+    }
+
+    public function testLoginIncorrectPassword(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/login');
+
+        $form = $crawler->selectButton('Submit')->form();
+        $form['username']->setValue($_ENV['ADMIN_LOGIN']);
+        $form['password']->setValue('bad_password');
+
+        $client->submit($form);
+        $this->assertResponseRedirects('/login');
+        $crawler = $client->followRedirect();
+        $this->assertResponseIsSuccessful();
+
+        $this->assertSelectorTextContains('div.alert.alert-danger', 'Invalid credentials.');
+    }
+
+    public function testLoginCorrect(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/login');
+
+        $form = $crawler->selectButton('Submit')->form();
+        $form['username']->setValue($_ENV['ADMIN_LOGIN']);
+        $form['password']->setValue($_ENV['ADMIN_PASSWORD']);
+
+        $client->submit($form);
+        $this->assertResponseRedirects('/dashboard');
+        $crawler = $client->followRedirect();
+        $this->assertResponseIsSuccessful();
+    }
+}
