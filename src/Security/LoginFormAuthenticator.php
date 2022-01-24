edbcd12049358606fc6cd8881e470442cd4b4242
tchapi <regbasket+github@gmail.com>
2022-01-24 23:07:02 +0100

    Fix use in authenticator

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -5,6 +5,7 @@ namespace App\Security;
 use App\Entity\AdminUser;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
