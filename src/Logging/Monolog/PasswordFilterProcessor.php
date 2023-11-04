db1ef336a234bb8ac20f6152f007e29e29456c55
tchapi <cyril.chapellier@alan.eu>
2023-11-04 13:54:43 +0100

    Better redaction

--- src/Logging/Monolog/PasswordFilterProcessor.php
+++ src/Logging/Monolog/PasswordFilterProcessor.php
@@ -6,19 +6,18 @@ use Monolog\Processor\ProcessorInterface;
 
 final class PasswordFilterProcessor implements ProcessorInterface
 {
+    private const REDACTED = '****';
     private const PASSWORD_KEY = 'password';
     private const SENSITIVE_ARGS_FUNCTIONS = ['validateUserPass', 'ldapOpen', 'password_verify', 'imapOpen', 'ldap_bind', 'hashPassword', 'dav'];
 
     public function __invoke(array $record): array
     {
+        // Remove potentially sensitive data from function arguments
+        $shouldRedactArgs = array_key_exists('function', $record) && in_array($record['function'], self::SENSITIVE_ARGS_FUNCTIONS);
+
         foreach ($record as $key => $item) {
-            if (self::PASSWORD_KEY === strtolower($key)) {
-                $record[$key] = '****';
-            } elseif ('function' === strtolower($key)) {
-                // Remove potentially sensitive data from function arguments
-                if (in_array($item, self::SENSITIVE_ARGS_FUNCTIONS)) {
-                    $record['args'] = ['****'];
-                }
+            if (self::PASSWORD_KEY === strtolower($key) || ('args' === $key && $shouldRedactArgs)) {
+                $record[$key] = self::REDACTED;
             } elseif (is_array($item)) {
                 $record[$key] = $this($item);
             }
