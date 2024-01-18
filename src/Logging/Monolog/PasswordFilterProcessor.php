fc40ed0cfcac8910e30a7942f1272e9406b238c1
tchapi <cyril.chapellier@alan.eu>
2024-01-18 23:56:23 +0100

    fix: filter processor

--- src/Logging/Monolog/PasswordFilterProcessor.php
+++ src/Logging/Monolog/PasswordFilterProcessor.php
@@ -2,27 +2,37 @@
 
 namespace App\Logging\Monolog;
 
+use Monolog\LogRecord;
 use Monolog\Processor\ProcessorInterface;
 
 final class PasswordFilterProcessor implements ProcessorInterface
 {
     private const REDACTED = '****';
-    private const PASSWORD_KEY = 'password';
+    private const PASSWORD_KEYS = ['pass', 'password'];
     private const SENSITIVE_ARGS_FUNCTIONS = ['validateUserPass', 'ldapOpen', 'password_verify', 'imapOpen', 'ldap_bind', 'hashPassword', 'dav'];
 
-    public function __invoke(array $record): array
+    public static function redactContextRecursive(array $context): array
     {
         // Remove potentially sensitive data from function arguments
-        $shouldRedactArgs = array_key_exists('function', $record) && in_array($record['function'], self::SENSITIVE_ARGS_FUNCTIONS);
+        $shouldRedactArgs = array_key_exists('function', $context) && in_array($context['function'], self::SENSITIVE_ARGS_FUNCTIONS);
 
-        foreach ($record as $key => $item) {
-            if (self::PASSWORD_KEY === strtolower($key) || ('args' === $key && $shouldRedactArgs)) {
-                $record[$key] = self::REDACTED;
+        foreach ($context as $key => $item) {
+            if (in_array(strtolower($key), self::PASSWORD_KEYS) || ('args' === $key && $shouldRedactArgs)) {
+                $context[$key] = self::REDACTED;
             } elseif (is_array($item)) {
-                $record[$key] = $this($item);
+                $context[$key] = static::redactContextRecursive($item);
             }
         }
 
-        return $record;
+        return $context;
+    }
+
+    public function __invoke(LogRecord $record): LogRecord
+    {
+        $context = $record->context;
+
+        $redactedContext = static::redactContextRecursive($context);
+
+        return $record->with(context: $redactedContext);
     }
 }
