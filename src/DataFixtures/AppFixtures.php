0a90d0b81d3b598820386dd32f6df08e7d8d5ab1
tchapi <cyril.chapellier@alan.eu>
2024-01-20 21:44:20 +0100

    chore: add setup for a better testing

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use App\Entity\Principal;
+use App\Entity\CalendarInstance;
+use App\Entity\Calendar;
+use App\Entity\AddressBook;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $hash = password_hash("password", PASSWORD_DEFAULT);
+        $user = (new User())
+            ->setUsername("test_user")
+            ->setPassword($hash);
+        $manager->persist($user);
+        
+        $principal = (new Principal())
+            ->setUri(Principal::PREFIX.$user->getUsername())
+            ->setEmail("test@test.com")
+            ->setDisplayName("Test User")
+            ->setIsAdmin(true);
+        $manager->persist($principal);
+
+        // Create all the default calendar / addressbook
+        $calendarInstance = new CalendarInstance();
+        $calendar = new Calendar();
+        $calendarInstance->setPrincipalUri(Principal::PREFIX.$user->getUsername())
+                    ->setUri('default')
+                    ->setDisplayName('default.calendar.title')
+                    ->setDescription('default.calendar.description')
+                    ->setCalendar($calendar);
+        $manager->persist($calendarInstance);
+
+        // Enable delegation by default
+        $principalProxyRead = new Principal();
+        $principalProxyRead->setUri($principal->getUri().Principal::READ_PROXY_SUFFIX)
+                            ->setIsMain(false);
+        $manager->persist($principalProxyRead);
+
+        $principalProxyWrite = new Principal();
+        $principalProxyWrite->setUri($principal->getUri().Principal::WRITE_PROXY_SUFFIX)
+                            ->setIsMain(false);
+        $manager->persist($principalProxyWrite);
+
+        $addressbook = new AddressBook();
+        $addressbook->setPrincipalUri(Principal::PREFIX.$user->getUsername())
+                    ->setUri('default')
+                    ->setDisplayName('default.addressbook.title')
+                    ->setDescription('default.addressbook.description');
+        $manager->persist($addressbook);
+        
+        $manager->flush();
+    }
+}
