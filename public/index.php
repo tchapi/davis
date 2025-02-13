8fd13c4293d99f8c94060eaed40b4e2f32b4afc5
Cyril Chapellier <tchapi@users.noreply.github.com>
2025-02-13 22:57:42 +0100

    [Chore] Update recipe files (#177)
    Co-authored-by: tchapi <regbasket@gmail.com>

--- public/index.php
+++ public/index.php
@@ -6,30 +6,12 @@ if (false !== getenv('ENV_DIR') && '' !== getenv('ENV_DIR')) {
 
 use App\Kernel;
 use Symfony\Component\Dotenv\Dotenv;
-use Symfony\Component\ErrorHandler\Debug;
-use Symfony\Component\HttpFoundation\Request;
 
-require dirname(__DIR__).'/vendor/autoload.php';
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-$env_dir = false != getenv('ENV_DIR') ? getenv('ENV_DIR') : dirname(__DIR__);
-(new Dotenv())->bootEnv($env_dir.'/.env');
+return function (array $context) {
+    $env_dir = false != getenv('ENV_DIR') ? getenv('ENV_DIR') : dirname(__DIR__);
+    (new Dotenv())->bootEnv($env_dir.'/.env');
 
-if ($_SERVER['APP_DEBUG']) {
-    umask(0000);
-
-    Debug::enable();
-}
-
-if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
-    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
-}
-
-if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
-    Request::setTrustedHosts([$trustedHosts]);
-}
-
-$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
