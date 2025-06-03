0355ae87f1e7fb03b3e8f91179d1af785ec6c959
tchapi <regbasket@gmail.com>
2025-06-03 22:41:59 +0200

    chore

--- public/index.php
+++ public/index.php
@@ -1,7 +1,11 @@
 <?php
 
-if (false !== getenv('ENV_DIR') && '' !== getenv('ENV_DIR')) {
-    $_SERVER['APP_RUNTIME_OPTIONS']['dotenv_path'] = getenv('ENV_DIR').'/.env';
+$overridenEnvDir = getenv('ENV_DIR') ?: null;
+
+if ($overridenEnvDir) {
+    // Tell the Runtime not to touch dotenv so we can load our own file.
+    // This is needed if the ENV_DIR is outside of the project directory
+    $_SERVER['APP_RUNTIME_OPTIONS']['disable_dotenv'] = true;
 }
 
 use App\Kernel;
@@ -9,9 +13,11 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-return function (array $context) {
-    $env_dir = false != getenv('ENV_DIR') ? getenv('ENV_DIR') : dirname(__DIR__);
-    (new Dotenv())->bootEnv($env_dir.'/.env');
+if ($overridenEnvDir) {
+    // Load our own now, after the runtime has booted
+    (new Dotenv())->bootEnv($overridenEnvDir.'/.env');
+}
 
+return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
