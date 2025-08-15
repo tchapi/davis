8cb61bec5c8f3c181a733134ae5077698365f8a6
tchapi <regbasket@gmail.com>
2025-08-15 23:57:48 +0200

    chore

--- src/Constants.php
+++ src/Constants.php
@@ -9,5 +9,5 @@ class Constants
     public const BIRTHDAY_CALENDAR_URI = 'birthday-calendar';
     public const BIRTHDAY_REMINDER_OFFSET = 'PT9H'; // 9am on the day of the event
 
-    public const MAX_DATE = '2038-01-01'; // Year 2038 bug — will be fixed by https://github.com/tchapi/davis/pull/186
+    public const MAX_DATE = '2222-01-01';
 }
