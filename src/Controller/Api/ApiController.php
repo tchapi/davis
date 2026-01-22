9bec3b1d7e6016a0aec4c4c1f1f21f13665e1191
Jakub <github@unkn0wnapi.me>
2026-01-22 17:21:38 +0100

    Added API endpoints for getting users, user's calendar and sharing calendar to users

--- src/Controller/Api/ApiController.php
+++ src/Controller/Api/ApiController.php
@@ -1,10 +1,18 @@
 <?php
 namespace App\Controller\Api;
 
+use App\Entity\Calendar;
+use App\Entity\CalendarInstance;
+use App\Entity\CalendarSubscription;
+
+use App\Entity\Principal;
+use App\Form\CalendarInstanceType;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
 
 #[Route('/api', name: 'api_')]
 class ApiController extends AbstractController
@@ -36,24 +44,30 @@ class ApiController extends AbstractController
      * Retrieves a list of users.
      *
      * @param Request $request The HTTP GET request
-     * @return JsonResponse A JSON response containing the list of users or an error message
+     * @return JsonResponse A JSON response containing the list of users, 
      */
     #[Route('/users', name: 'users', methods: ['GET'])]
-    public function getUsers(Request $request): JsonResponse
+    public function getUsers(Request $request, ManagerRegistry $doctrine): JsonResponse
     {
         if (!$this->validateApiKey($request)) {
             return $this->json(['error' => 'Unauthorized'], 401);
         }
 
-        // Dummy data for demonstration purposes
-        $users = [
-            [
-                "username" => "johndoe",
-                "displayname" => "John Doe",
-                "email" => "johndoe@example.com",
-                "uri" => "principals/johndoe"
-            ]
-        ];
+        $principals = $doctrine->getRepository(Principal::class)->findByIsMain(true);
+
+        if (!$principals) {
+            return $this->json(['status' => 'success', 'data' => []], 200);
+        }
+
+        foreach ($principals as $principal) {
+            $users[] = [
+                "id" => $principal->getId(),
+                "uri" => $principal->getUri(),
+                "username" => $principal->getUsername(),
+                "displayname" => $principal->getDisplayName(),
+                "email" => $principal->getEmail(),
+            ];
+        }
 
         $response = [
             "status" => "success",
@@ -63,24 +77,112 @@ class ApiController extends AbstractController
         return $this->json($response, 200);
     }
 
+    /**
+     * Retrieves a list of calendars for a specific user.
+     *
+     * @param Request $request The HTTP GET request
+     * @param string $username The username of the user whose calendars are to be retrieved
+     * @return JsonResponse A JSON response containing the list of calendars
+     */
     #[Route('/calendars/{username}', name: 'calendars', methods: ['GET'])]
-    public function getUserCalendars(Request $request, string $username): JsonResponse
+    public function getUserCalendars(Request $request, string $username, ManagerRegistry $doctrine): JsonResponse
     {
         if (!$this->validateApiKey($request)) {
-            return $this->json(['error' => 'Unauthorized'], 401);
+            return $this->json(['status' => 'Error', 'message' => 'Unauthorized'], 401);
         }
 
-        // Dummy data for demonstration purposes
-        $calendars = [
-            [
-                "displayname" => "Personal Calendar",
-                "uri" => "default",
-                "description" => "Work related events",
-                "events" => 1,
-                "notes" => 1,
-                "tasks" => 1
-            ],
+        if (empty($username) || !is_string($username) || preg_match('/[^a-zA-Z0-9_-]/', $username)) {
+            return $this->json(['status' => 'Error', 'message' => 'Invalid Username'], 400);
+        }
+
+        $allCalendars = $doctrine->getRepository(CalendarInstance::class)->findByPrincipalUri(Principal::PREFIX.$username);
+        $subscriptions = $doctrine->getRepository(CalendarSubscription::class)->findByPrincipalUri(Principal::PREFIX.$username);
+
+        if (!$allCalendars && !$subscriptions) {
+            return $this->json(['status' => 'success', 'data' => []], 200);
+        }
+
+        foreach ($allCalendars as $calendar) {
+            if (!$calendar->isShared()) {
+                $calendars[] = [
+                    "displayname" => $calendar->getDisplayName(),
+                    "uri" => $calendar->getUri(),
+                    "id" => $calendar->getId(),
+                    "description" => $calendar->getDescription(),
+                    "events" => count($calendar->getCalendar()->getObjects()->filter(fn($obj) => $obj->getComponentType() === Calendar::COMPONENT_EVENTS)),
+                    "notes" => count($calendar->getCalendar()->getObjects()->filter(fn($obj) => $obj->getComponentType() === Calendar::COMPONENT_NOTES)),
+                    "tasks" => count($calendar->getCalendar()->getObjects()->filter(fn($obj) => $obj->getComponentType() === Calendar::COMPONENT_TODOS)),
+                ];
+            } else {
+                $sharedCalendars[] = [
+                    "displayname" => $calendar->getDisplayName(),
+                    "uri" => $calendar->getUri(),
+                    "id" => $calendar->getId(),
+                    "description" => $calendar->getDescription(),
+                    "events" => count($calendar->getCalendar()->getObjects()->filter(fn($obj) => $obj->getComponentType() === Calendar::COMPONENT_EVENTS)),
+                    "notes" => count($calendar->getCalendar()->getObjects()->filter(fn($obj) => $obj->getComponentType() === Calendar::COMPONENT_NOTES)),
+                    "tasks" => count($calendar->getCalendar()->getObjects()->filter(fn($obj) => $obj->getComponentType() === Calendar::COMPONENT_TODOS)),
+                ];
+            }
+        }
+
+        foreach ($subscriptions as $subscription) {
+            $calendars[] = [
+                "displayname" => $subscription->getDisplayName(),
+                "uri" => $subscription->getUri(),
+                "description" => $subscription->getDescription(),
+                "events" => count($subscription->getCalendar()->getObjects()->filter(fn($obj) => $obj->getComponentType() === Calendar::COMPONENT_EVENTS)),
+                "notes" => count($subscription->getCalendar()->getObjects()->filter(fn($obj) => $obj->getComponentType() === Calendar::COMPONENT_NOTES)),
+                "tasks" => count($subscription->getCalendar()->getObjects()->filter(fn($obj) => $obj->getComponentType() === Calendar::COMPONENT_TODOS)),
+            ];
+        }
+
+        $response = [
+            "status" => "success",
+            "data" => [
+                "user_calendars" => $calendars ?? [],
+                "other_calendars" => $sharedCalendars ?? [],
+                "subscriptions" => $subscriptions ?? []
+            ]
         ];
+
+        return $this->json($response, 200);
+    }
+
+    /**
+     * Retrieves a list of shares for a specific calendar of a specific user.
+     *
+     * @param Request $request The HTTP GET request
+     * @param string $username The username of the user whose calendar shares are to be retrieved
+     * @param string $calendar_id The ID of the calendar whose shares are to be retrieved
+     * @return JsonResponse A JSON response containing the list of calendar shares
+     */
+    #[Route('/calendars/{username}/shares/{calendar_id}', name: 'calendars_shares', methods: ['GET'])]
+    public function getUserCalendarsShares(Request $request, string $username, int $calendar_id, ManagerRegistry $doctrine) : JsonResponse
+    {
+        if (!$this->validateApiKey($request)) {
+            return $this->json(['status' => 'Error', 'message' => 'Unauthorized'], 401);
+        }
+
+        if (!is_string($username) || preg_match('/[^a-zA-Z0-9_-]/', $username) || !is_int($calendar_id)) {
+            return $this->json(['status' => 'Error', 'message' => 'Invalid Username/Calendar ID'], 400);
+        }
+
+        // TODO: Either wrong Id is return or sth else is wrong here
+        $instances = $doctrine->getRepository(CalendarInstance::class)->findSharedInstancesOfInstance($calendar_id, true);
+
+        if (!$instances) {
+            return $this->json(['status' => 'success', 'data' => []], 200);
+        }
+        
+        foreach ($instances as $instance) {
+            $calendars[] = [
+                'username' => mb_substr($instance[0]['principalUri'], strlen(Principal::PREFIX)),
+                'displayname' => $instance['displayName'],
+                'email' => $instance['email'],
+                'write_access' => CalendarInstance::ACCESS_READWRITE === $instance[0]['access'],
+            ];
+        }
 
         $response = [
             "status" => "success",
@@ -90,26 +192,49 @@ class ApiController extends AbstractController
         return $this->json($response, 200);
     }
 
-    #[Route('/addressbooks/{username}', name: 'addressbooks', methods: ['GET'])]
-    public function getUserAddressBooks(Request $request, string $username): JsonResponse
+    #[Route('/calendars/{username}/share/{calendar_id}', name: 'calendars_share', methods: ['POST'])]
+    public function setUserCalendarsShare(Request $request, string $username, string $calendar_id, ManagerRegistry $doctrine): JsonResponse
     {
         if (!$this->validateApiKey($request)) {
-            return $this->json(['error' => 'Unauthorized'], 401);
+            return $this->json(['status' => 'Error', 'message' => 'Unauthorized'], 401);
         }
 
-        // Dummy data for demonstration purposes
-        $addressBooks = [
-            [
-                "displayname" => "Personal Contacts",
-                "uri" => "default",
-                "description" => "My personal contacts",
-                "cards" => 10
-            ],
-        ];
+        if (!is_string($username) || preg_match('/[^a-zA-Z0-9_-]/', $username) || !is_numeric($calendar_id)) {
+            return $this->json(['status' => 'Error', 'message' => 'Invalid Username/Calendar ID'], 400);
+        }
+
+        $instance = $doctrine->getRepository(CalendarInstance::class)->findOneById($calendar_id);
+        $newShareeToAdd = $doctrine->getRepository(Principal::class)->findOneById($request->get('id'));
+
+        if (!$instance || !$newShareeToAdd) {
+            return $this->json(['status' => 'Error', 'message' => 'Calendar Instance/User Not Found'], 404);
+        }
+
+        $existingSharedInstance = $doctrine->getRepository(CalendarInstance::class)->findSharedInstanceOfInstanceFor($instance->getCalendar()->getId(), $newShareeToAdd->getUri());
+
+        $writeAccess = ('true' === $request->get('write') ? CalendarInstance::ACCESS_READWRITE : CalendarInstance::ACCESS_READ);
+
+        $entityManager = $doctrine->getManager();
+
+        if ($existingSharedInstance) {
+            $existingSharedInstance->setAccess($writeAccess);
+        } else {
+            $sharedInstance = new CalendarInstance();
+            $sharedInstance->setTransparent(1)
+                     ->setCalendar($instance->getCalendar())
+                     ->setShareHref('mailto:'.$newShareeToAdd->getEmail())
+                     ->setDescription($instance->getDescription())
+                     ->setDisplayName($instance->getDisplayName())
+                     ->setUri(\Sabre\DAV\UUIDUtil::getUUID())
+                     ->setPrincipalUri($newShareeToAdd->getUri())
+                     ->setAccess($writeAccess);
+            $entityManager->persist($sharedInstance);
+        }
+
+        $entityManager->flush();
 
         $response = [
-            "status" => "success",
-            "data" => $addressBooks
+            "status" => "success"
         ];
 
         return $this->json($response, 200);
