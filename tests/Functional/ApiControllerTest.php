b24ef150ca8ab490d37a8846cb294efda0f6f505
Jakub <github@unkn0wnapi.me>
2026-02-04 18:26:13 +0100

    ApiController PHPUnit Tests - Part 2

--- tests/Functional/ApiControllerTest.php
+++ tests/Functional/ApiControllerTest.php
@@ -6,17 +6,59 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ApiControllerTest extends WebTestCase
 {
-    private function getUserUsername($client): string {
+    /*
+     * Helper function to get an existing username from the user list
+     * 
+     * @param mixed $client
+     * 
+     * @return string Username
+     */
+    private function getUserUsername($client): string
+    {
         $client->request('GET', '/api/v1/users', [], [], [
             'HTTP_ACCEPT' => 'application/json',
-            'HTTP_X_DAVIS_API_TOKEN' => $_ENV['API_KEY']
+            'HTTP_X_DAVIS_API_TOKEN' => $_ENV['API_KEY'],
         ]);
-        
+
         $this->assertResponseIsSuccessful();
         $this->assertResponseHeaderSame('Content-Type', 'application/json');
         
         $data = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertArrayHasKey('data', $data);;
+        $this->assertIsArray($data['data']);
+        $this->assertStringContainsString('test_user', $data['data'][0]['username']);
+
         return $data['data'][0]['username'];
+    }
+
+    /*
+     * Helper function to get an existing calendar ID from the user calendar list
+     * 
+     * @param mixed  $client
+     * @param string $username
+     * @param bool   $default  Whether to get the default calendar (true) or the second calendar (false)
+     * 
+     * @return int Calendar ID
+     */
+    private function getCalendarId($client, string $username, bool $default = true): int
+    {
+        $client->request('GET', '/api/v1/calendars/'.$username, [], [], [
+            'HTTP_ACCEPT' => 'application/json',
+            'HTTP_X_DAVIS_API_TOKEN' => $_ENV['API_KEY'],
+        ]);
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseHeaderSame('Content-Type', 'application/json');
+
+        $data = json_decode($client->getResponse()->getContent(), true);
+        $this->assertIsArray($data);
+        $this->assertNotEmpty($data);
+
+        if ($default) {
+            return $data['data']['user_calendars'][0]['id'];
+        } else {
+            return $data['data']['user_calendars'][1]['id'];
+        }
     }
 
     /*
@@ -26,10 +68,10 @@ class ApiControllerTest extends WebTestCase
     {
         $client = static::createClient();
         $client->request('GET', '/api/v1/health');
-        
+
         $this->assertResponseIsSuccessful();
         $this->assertResponseHeaderSame('Content-Type', 'application/json');
-        
+
         $data = json_decode($client->getResponse()->getContent(), true);
         $this->assertEquals('OK', $data['status']);
     }
@@ -37,40 +79,44 @@ class ApiControllerTest extends WebTestCase
     /*
      * Test the user list endpoint
      */
-    public function testUserList(): void {
+    public function testUserList(): void
+    {
         $client = static::createClient();
         $client->request('GET', '/api/v1/users', [], [], [
             'HTTP_ACCEPT' => 'application/json',
-            'HTTP_X_DAVIS_API_TOKEN' => $_ENV['API_KEY']
+            'HTTP_X_DAVIS_API_TOKEN' => $_ENV['API_KEY'],
         ]);
-        
+
         $this->assertResponseIsSuccessful();
         $this->assertResponseHeaderSame('Content-Type', 'application/json');
-        
+
         $data = json_decode($client->getResponse()->getContent(), true);
         $this->assertIsArray($data);
         $this->assertNotEmpty($data);
-        
+
         // Check if user is present in db
         $this->assertArrayHasKey('id', $data['data'][0]);
         $this->assertArrayHasKey('uri', $data['data'][0]);
+        $this->assertStringContainsString('principals/test_user', $data['data'][0]['uri']);
         $this->assertArrayHasKey('username', $data['data'][0]);
+        $this->assertStringContainsString('test_user', $data['data'][0]['username']);
     }
 
     /*
      * Test the user details endpoint
      */
-    public function testUserDetails(): void {
+    public function testUserDetails(): void
+    {
         // Create client once
         $client = static::createClient();
-        
+
         // Get username from existing user lists
         $username = $this->getUserUsername($client);
-        
+
         // Check user details endpoint
-        $client->request('GET', '/api/v1/users/' . $username, [], [], [
+        $client->request('GET', '/api/v1/users/'.$username, [], [], [
             'HTTP_ACCEPT' => 'application/json',
-            'HTTP_X_DAVIS_API_TOKEN' => $_ENV['API_KEY']
+            'HTTP_X_DAVIS_API_TOKEN' => $_ENV['API_KEY'],
         ]);
         $this->assertResponseIsSuccessful();
         $this->assertResponseHeaderSame('Content-Type', 'application/json');
@@ -81,20 +127,23 @@ class ApiControllerTest extends WebTestCase
 
         // Check if user details are correct
         $this->assertArrayHasKey('displayname', $data['data']);
+        $this->assertStringContainsString('Test User', $data['data']['displayname']);
         $this->assertArrayHasKey('email', $data['data']);
+        $this->assertStringContainsString('test@test.com', $data['data']['email']);
         $this->assertStringEqualsStringIgnoringLineEndings($username, $data['data']['username']);
     }
 
     /*
      * Test the user calendars list endpoint
      */
-    public function testUserCalendarsList(): void {
+    public function testUserCalendarsList(): void
+    {
         $client = static::createClient();
         $username = $this->getUserUsername($client);
 
-        $client->request('GET', '/api/v1/calendars/' . $username, [], [], [
+        $client->request('GET', '/api/v1/calendars/'.$username, [], [], [
             'HTTP_ACCEPT' => 'application/json',
-            'HTTP_X_DAVIS_API_TOKEN' => $_ENV['API_KEY']
+            'HTTP_X_DAVIS_API_TOKEN' => $_ENV['API_KEY'],
         ]);
         $this->assertResponseIsSuccessful();
         $this->assertResponseHeaderSame('Content-Type', 'application/json');
@@ -107,18 +156,24 @@ class ApiControllerTest extends WebTestCase
         $this->assertArrayHasKey('status', $data);
         $this->assertEquals('success', $data['status']);
         $this->assertArrayHasKey('user_calendars', $data['data']);
+        $this->assertStringContainsString('default', $data['data']['user_calendars'][0]['uri']);
+        $this->assertStringContainsString('default.calendar.title', $data['data']['user_calendars'][0]['displayname']);
         $this->assertArrayHasKey('shared_calendars', $data['data']);
         $this->assertArrayHasKey('subscriptions', $data['data']);
     }
 
-    public function testUserCalendarDetails(): void {
+    /*
+     * Test the user calendar details endpoint
+     */
+    public function testUserCalendarDetails(): void
+    {
         $client = static::createClient();
         $username = $this->getUserUsername($client);
 
         // Get calendar list to retrieve calendar ID
-        $client->request('GET', '/api/v1/calendars/' . $username, [], [], [
+        $client->request('GET', '/api/v1/calendars/'.$username, [], [], [
             'HTTP_ACCEPT' => 'application/json',
-            'HTTP_X_DAVIS_API_TOKEN' => $_ENV['API_KEY']
+            'HTTP_X_DAVIS_API_TOKEN' => $_ENV['API_KEY'],
         ]);
         $this->assertResponseIsSuccessful();
         $this->assertResponseHeaderSame('Content-Type', 'application/json');
@@ -128,11 +183,11 @@ class ApiControllerTest extends WebTestCase
         $this->assertNotEmpty($data);
 
         $calendar_id = $data['data']['user_calendars'][0]['id'];
-        
+
         // Check calendar details endpoint
-        $client->request('GET', '/api/v1/calendars/' . $username . '/' . $calendar_id, [], [], [
+        $client->request('GET', '/api/v1/calendars/'.$username.'/'.$calendar_id, [], [], [
             'HTTP_ACCEPT' => 'application/json',
-            'HTTP_X_DAVIS_API_TOKEN' => $_ENV['API_KEY']
+            'HTTP_X_DAVIS_API_TOKEN' => $_ENV['API_KEY'],
         ]);
         $this->assertResponseIsSuccessful();
         $this->assertResponseHeaderSame('Content-Type', 'application/json');
@@ -141,12 +196,17 @@ class ApiControllerTest extends WebTestCase
         $this->assertIsArray($data);
         $this->assertNotEmpty($data);
 
+
         // Check if calendar details are correct
         $this->assertArrayHasKey('id', $data['data']);
         $this->assertArrayHasKey('uri', $data['data']);
+        $this->assertStringContainsString('default', $data['data']['uri']);
         $this->assertArrayHasKey('displayname', $data['data']);
+        $this->assertStringContainsString('default.calendar.title', $data['data']['displayname']);
+
         $this->assertArrayHasKey('description', $data['data']);
-        
+        $this->assertStringContainsString('default.calendar.description', $data['data']['description']);
+
         $this->assertArrayHasKey('events', $data['data']);
         $this->assertIsArray($data['data']['events']);
         $this->assertArrayHasKey('notes', $data['data']);
@@ -155,10 +215,123 @@ class ApiControllerTest extends WebTestCase
         $this->assertIsArray($data['data']['tasks']);
     }
 
-    // TODO: TestCreateUser
-    // TODO: TestShareCalendarToNewUser
+    /*
+     * Test creating a new user calendar
+     */
+    public function testCreateUserCalendar(): void {
+        $client = static::createClient();
+        $username = $this->getUserUsername($client);
+
+        // Create user API request with JSON body
+        $payload = [
+            'uri' => 'api_calendar',
+            'name' => 'api.calendar.title',
+            'description' => 'api.calendar.description',
+            'events_support' => true,
+            'tasks_support' => true,
+            'notes_support' => false,
+        ];
+
+        $client->request('POST', '/api/v1/calendars/'.$username.'/create', [], [], [
+            'HTTP_ACCEPT' => 'application/json',
+            'HTTP_X_DAVIS_API_TOKEN' => $_ENV['API_KEY'],
+            'CONTENT_TYPE' => 'application/json',
+        ], json_encode($payload));
+
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseHeaderSame('Content-Type', 'application/json');
+
+        $data = json_decode($client->getResponse()->getContent(), true);
+        
+        $this->assertIsArray($data);
+        $this->assertArrayHasKey('status', $data);
+        $this->assertEquals('success', $data['status']);
+        $this->assertArrayHasKey('data', $data);
+        $this->assertMatchesRegularExpression('/^\d+$/', $data['data']['calendar_id']);
+        $this->assertStringContainsString('api_calendar', $data['data']['calendar_uri']);
+        
+        // Check if calendar is created
+        $calendarId = $data['data']['calendar_id'];
+        $client->request('GET', '/api/v1/calendars/'.$username.'/'.$calendarId, [], [], [
+            'HTTP_ACCEPT' => 'application/json',
+            'HTTP_X_DAVIS_API_TOKEN' => $_ENV['API_KEY'],
+        ]);
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseHeaderSame('Content-Type', 'application/json');
+
+        $data = json_decode($client->getResponse()->getContent(), true);
+        $this->assertIsArray($data);
+        $this->assertNotEmpty($data);
+
+        $this->assertArrayHasKey('events', $data['data']);
+        $this->assertIsBool($data['data']['events']['enabled']);
+        $this->assertTrue($data['data']['events']['enabled']);
+        $this->assertArrayHasKey('tasks', $data['data']);
+        $this->assertIsBool($data['data']['tasks']['enabled']);
+        $this->assertTrue($data['data']['tasks']['enabled']);
+        $this->assertArrayHasKey('notes', $data['data']);
+        $this->assertIsBool($data['data']['notes']['enabled']);
+        $this->assertFalse($data['data']['notes']['enabled']);
+    }
+
+    /*
+     * Test editing a user calendar
+     */
+    public function testEditUserCalendar(): void {
+        $client = static::createClient();
+        $username = $this->getUserUsername($client);
+        $calendar_id = $this->getCalendarId($client, $username, true);
+
+        // Edit user default calendar
+        $payload = [
+            'name' => 'api.calendar.edited.title',
+            'description' => 'api.calendar.edited.description',
+            'events_support' => true,
+            'tasks_support' => true,
+            'notes_support' => true,
+        ];
+        $client->request('POST', '/api/v1/calendars/'.$username.'/'.$calendar_id.'/edit', [], [], [
+            'HTTP_ACCEPT' => 'application/json',
+            'HTTP_X_DAVIS_API_TOKEN' => $_ENV['API_KEY'],
+            'CONTENT_TYPE' => 'application/json',
+        ], json_encode($payload));
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseHeaderSame('Content-Type', 'application/json');
+
+        $data = json_decode($client->getResponse()->getContent(), true);
+        $this->assertIsArray($data);
+        $this->assertArrayHasKey('status', $data);
+        $this->assertEquals('success', $data['status']);
+
+        // Check if edits were applied
+        $client->request('GET', '/api/v1/calendars/'.$username.'/'.$calendar_id, [], [], [
+            'HTTP_ACCEPT' => 'application/json',
+            'HTTP_X_DAVIS_API_TOKEN' => $_ENV['API_KEY'],
+        ]);
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseHeaderSame('Content-Type', 'application/json');
+
+        $data = json_decode($client->getResponse()->getContent(), true);
+        $this->assertIsArray($data);
+        $this->assertArrayHasKey('data', $data);
+        $this->assertIsArray($data['data']);
+        $this->assertStringContainsString($payload['name'], $data['data']['displayname']);
+        $this->assertStringContainsString($payload['description'], $data['data']['description']);
+        
+        $this->assertArrayHasKey('events', $data['data']);
+        $this->assertIsBool($data['data']['events']['enabled']);
+        $this->assertTrue($data['data']['events']['enabled']);
+        $this->assertArrayHasKey('tasks', $data['data']);
+        $this->assertIsBool($data['data']['tasks']['enabled']);
+        $this->assertTrue($data['data']['tasks']['enabled']);
+        $this->assertArrayHasKey('notes', $data['data']);
+        $this->assertIsBool($data['data']['notes']['enabled']);
+        $this->assertTrue($data['data']['notes']['enabled']);
+    }
+
+
+    // TODO: TestShareCalendarToUser
     // TODO: TestShareCalendarList
-    // TODO: TestUnshareCalendarToNewUser
-    // TODO: TestCreateCalendarForUser
-    // TODO: TestRemoveCalendarForUser
+    // TODO: TestUnshareCalendarToUser
+    // TODO: TestEditCalendarForUser
 }
