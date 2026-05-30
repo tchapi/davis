5830f1cb7a1be8f2942e63d0de2092f691d3d59f
tchapi <regbasket@gmail.com>
2026-05-30 22:50:10 +0200

    chore

--- src/Controller/Admin/CalendarController.php
+++ src/Controller/Admin/CalendarController.php
@@ -224,6 +224,7 @@ class CalendarController extends AbstractController
                      ->setShareHref('mailto:'.$newShareeToAdd->getEmail())
                      ->setDescription($instance->getDescription())
                      ->setDisplayName($instance->getDisplayName())
+                     ->setCalendarColor($instance->getCalendarColor())
                      ->setUri(\Sabre\DAV\UUIDUtil::getUUID())
                      ->setPrincipalUri($newShareeToAdd->getUri())
                      ->setAccess($writeAccess);
